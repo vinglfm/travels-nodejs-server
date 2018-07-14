8772fc502877a0a05ea95b7fe42928440c40eba3
Andrew Pryshchepa <vinglfm@gmail.com>
2018-07-14 19:18:27 +0300

    Added sign up endpoint

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Main page');
 });
 
-app.post('/users/authenticate', (req, res) => {
+app.post('/users/authenticate/signIn', (req, res) => {
     res.json({ 
-        user: 'New',
+        user: req.body.userName,
         token: 5,
         firstName: 'And',
         lastName: 'mescx'
       });
 });
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'))
+app.post('/users/authenticate/signUp', (req, res) => {
+    res.json({ 
+        user: req.body.userName,
+        token: 5,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      });
+});
+
+app.listen(4000, () => console.log('Travels server is running on port 4000!'))
