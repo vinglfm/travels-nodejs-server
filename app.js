775ebe288d4178b72433adb638a2e7486458eb15
Andrew Pryshchepa <vinglfm@gmail.com>
2018-07-22 12:54:13 +0300

    Moving authorization to sepparate module

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config');
-const User = require('./model/user');
 const app = express();
+const authorization = require('./common/authorization');
 const authRouter = require('./routes/auth');
 const echoRouter = require('./routes/echo');
-
-const authorization = (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if(token) {
-        jwt.verify(token, config.auth.secret, function(err, decoded) {
-            if(err) {
-                return next(err);
-            } else {
-                next();
-            }
-        });    
-    } else {
-      const err = new Error('Token is not specified');
-      err.status = 400;
-      return next(err);
-    }
-};
 
 mongoose.connect(`mongodb+srv://${config.mongo.user}:${config.mongo.password}@${config.mongo.url}`);
 const db = mongoose.connection;
---
+++ common/authorization.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+module.exports = (req, res, next) => {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if(token) {
+        jwt.verify(token, config.auth.secret, function(err, decoded) {
+            if(err) {
+                return next(err);
+            } else {
+                next();
+            }
+        });    
+    } else {
+      const err = new Error('Token is not specified');
+      err.status = 400;
+      return next(err);
+    }
+};
