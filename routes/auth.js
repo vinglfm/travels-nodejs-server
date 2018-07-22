6aa8720a19cc5a291bddff8884b389269a93d87c
Andrew Pryshchepa <vinglfm@gmail.com>
2018-07-22 18:11:07 +0300

    Added fb sign in endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const User = require('../model/user');
 const config = require('../config');
+require('../common/passport')();
 
 const maskPassword = (password) => {
     return config.auth.mask + password.toUpperCase().substr(4);
@@ -32,6 +34,17 @@ router.post('/signIn', (req, res, next) => {
             });
     }});
 });
+
+router.route('/facebook')
+    .post(passport.authenticate('facebook-token', {session: false}), function(req, res, next) {
+        if (!req.user) {
+            return res.send(401, 'User Not Authenticated');
+        }
+
+        const token = generateToken(req.user.email, req.user.email);
+        res.setHeader('x-auth-token', token);
+        return res.status(200).json(req.user);
+    });
 
 router.post('/signUp', (req, res, next) => {
     User.create({
