e65bb880a0220b8d922fa9d1412e36e1bea4abf7
Andrew Pryshchepa <vinglfm@gmail.com>
2018-07-28 15:28:29 +0300

    Checking user on authorization

--- common/authorization.js
+++ common/authorization.js
@@ -4,10 +4,17 @@ const config = require('../config');
 module.exports = (req, res, next) => {
     const token = req.body.token || req.query.token || req.headers['x-access-token'];
     if(token) {
+        const user = req.body.user || req.query.user;
         jwt.verify(token, config.auth.secret, function(err, decoded) {
+            console.log(decoded);
             if(err) {
                 return next(err);
-            } else {
+            } else if(decoded.user !== user) {
+                const err = new Error('Access token is not valid');
+                err.status = 400;
+                return next(err);
+            }else {
+                req.token = token;
                 next();
             }
         });    
